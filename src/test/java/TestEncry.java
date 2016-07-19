0ebbe2ea5bbb7adcdad4c2851fce36f1007e8250
hkohen <hkohen8@gmail.com>
2016-07-19 09:22:52 +0100

    encryptor class

---
+++ src/test/java/TestEncry.java
@@ -0,0 +1,17 @@
+import com.Encryptor;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by hk on 18/07/2016.
+ */
+public class TestEncry {
+
+    @Test
+    public void test() throws NoSuchAlgorithmException {
+        System.out.println(Encryptor.encrypte("hello world"));
+        Assert.assertTrue(Encryptor.testPassword("hello world","5eb63bbbe01eeed093cb2bb8f5acdc3"));
+    }
+}
